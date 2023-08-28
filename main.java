06d792db50905e62ae202e4126b29d787bf7b686
BharatiChauhan <chauhanbharati9951@gmail.com>
2023-08-28 17:04:57 +0530

    commit2

--- main.java
+++ main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args ){
 
         System.out.println("hello... ");
-        System.out.println("This is a demo for github.. ");
+        System.out.println("github demo ");
 
     }
 }
