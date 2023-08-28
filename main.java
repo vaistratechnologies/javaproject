9cabfcb11ed37824efe4f8d7a167e6838b12ff74
Dhara <jishidhara2003@gmail.com>
2023-08-28 17:06:13 +0530

    1st commit

--- main.java
+++ main.java
@@ -5,7 +5,7 @@ public class main {
 
         System.out.println("Hello world!");
         System.out.println("This is a demo project");
-        System.out.println("this is fist file");
+
 
 
     }
