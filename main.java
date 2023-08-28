4cbfee2edbe274dd51cf8289c576363a11e151ba
DharaaJoshi <143092471+DharaaJoshi@users.noreply.github.com>
2023-08-28 16:02:28 +0530

    Update main.java

--- main.java
+++ main.java
@@ -5,7 +5,7 @@ public class main {
 
         System.out.println("Hello world!");
         System.out.println("This is a demo project");
-        System.out.println("This is a demo ap");
+        System.out.println("This is a demo application");
 
     }
 }
