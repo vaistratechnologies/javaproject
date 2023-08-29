909ef69725834fc54fb18e0c59e9622da72fdc81
dmthanki <129146996+dmthanki@users.noreply.github.com>
2023-08-29 09:58:36 +0530

    Delete src directory

--- src/main/java/com/contacts/exception/CustomUniqueConstrainException.java
+++
@@ -1,7 +0,0 @@
-package com.contacts.exception;
-
-public class CustomUniqueConstrainException extends Throwable {
-    public CustomUniqueConstrainException(String s) {
-        super(s);
-    }
-}
