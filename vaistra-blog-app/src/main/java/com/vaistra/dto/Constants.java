65104245b9283a99d9471c369b62e84aa03a46af
BharatiChauhan <142868627+BharatiChauhan@users.noreply.github.com>
2023-08-29 10:29:56 +0530

    Delete vaistra-blog-app directory

--- vaistra-blog-app/src/main/java/com/vaistra/dto/Constants.java
+++
@@ -1,5 +0,0 @@
-package com.vaistra.dto;
-
-public class Constants {
-    public static final String[] PUBLIC_URLS= {"/user/**", "/swagger-ui/**"};
-}
--- vaistra-blog-app/src/main/java/com/vaistra/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.vaistra.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String msg) {
-        super(msg);
-    }
-}
